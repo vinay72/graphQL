1f5aebb9ebd64d42559bb2f7eb41915c6b9d93de
Vinay Sagar Sharma <vinaysagar971995@gmail.com>
2019-02-12 02:57:43 +0530

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,7 +6,7 @@ export default ({ data }) => (
 	<Layout>
     <h1>About {data.site.siteMetadata.title}</h1>    
      <p>
-      We're the only site running on your computer dedicated to showing the best
+      We are the only site running on your computer dedicated to showing the best
       photos and videos of pandas eating lots of food.
     </p>
   </Layout>
