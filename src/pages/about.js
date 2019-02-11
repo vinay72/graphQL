9123a16580cb54138d151aa018978a3562a28f62
Vinay Sagar Sharma <vinaysagar971995@gmail.com>
2019-02-12 02:57:26 +0530

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,22 @@
 import React from "react"
+import { graphql } from "gatsby"
 import Layout from "../components/layout"
 
-export default () => (
-  <Layout>
-    <h1>About Pandas Eating Lots</h1>
-    <p>
+export default ({ data }) => (  
+	<Layout>
+    <h1>About {data.site.siteMetadata.title}</h1>    
+     <p>
       We're the only site running on your computer dedicated to showing the best
       photos and videos of pandas eating lots of food.
     </p>
   </Layout>
 )
+
+export const query = graphql`  
+query {   
+ site {     
+      siteMetadata {    
+      title     
+    }   
+  } 
+}`
