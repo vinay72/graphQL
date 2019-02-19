d2a83d2aefcaebb14c9782d690e3af5fe75ddf27
Vinay Sagar Sharma <vinaysagar971995@gmail.com>
2019-02-20 03:48:09 +0530

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,15 @@
 module.exports = {
-  siteMetadata: {   
+	siteMetadata: {   
 	 title: `Pandas Eating Lots`,  
 	},
   plugins: [
+  {     
+   resolve: `gatsby-source-filesystem`,   
+      options: {    
+          name: `src`,    
+            path: `${__dirname}/src/`,  
+        },  
+    },
     `gatsby-plugin-emotion`,
     {
       resolve: `gatsby-plugin-typography`,
