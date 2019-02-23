847ccc4861865caf3cb851849f44c6bfa57ac765
Vinay Sagar Sharma <vinaysagar971995@gmail.com>
2019-02-23 12:10:54 +0530

    Update gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -31,7 +31,7 @@ exports.createPages = ({ graphql, actions }) => {
     result.data.allMarkdownRemark.edges.forEach(({ node }) => {      
     createPage({     
       path: node.fields.slug,       
-      component: path.resolve(`./src/templates/blog-post.js`),    
+      component: path.resolve(`./src/template/blog-post.js`),    
       context: {        
         // Data passed to context is available       
         // in page queries as GraphQL variables.  
