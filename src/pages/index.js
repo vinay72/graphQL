450577580b03889d0b20af8ca160ec4430acb288
Vinay Sagar Sharma <vinaysagar971995@gmail.com>
2019-02-23 12:38:32 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { graphql } from "gatsby"
+import { Link, graphql } from "gatsby"
 import { css } from "@emotion/core"
 import { rhythm } from "../utils/typography"
 import Layout from "../components/layout"
@@ -20,6 +20,14 @@ export default ({ data }) => {
         <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
         {data.allMarkdownRemark.edges.map(({ node }) => (
           <div key={node.id}>
+          <Link            
+            to={node.fields.slug}   
+              css={css` 
+                text-decoration: none;        
+                color: inherit;              
+              `
+            }            
+          >
             <h3
               css={css`
                 margin-bottom: ${rhythm(1 / 4)};
@@ -35,6 +43,7 @@ export default ({ data }) => {
               </span>
             </h3>
             <p>{node.excerpt}</p>
+            </Link> 
           </div>
         ))}
       </div>
@@ -51,6 +60,9 @@ export const query = graphql`
           frontmatter {
             title
             date(formatString: "DD MMMM, YYYY")
+          }
+          fields {   
+            slug          
           }
           excerpt
         }
