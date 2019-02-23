03ed48fa25e4ca0e9321240a915c895db8b200d4
Vinay Sagar Sharma <vinaysagar971995@gmail.com>
2019-02-23 12:05:37 +0530

    Update gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -14,7 +14,8 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
 }
 
 exports.createPages = ({ graphql, actions }) => {
-  const { createPage } = actions  return graphql(`
+  const { createPage } = actions 
+  return graphql(`
     {
       allMarkdownRemark {
         edges {
