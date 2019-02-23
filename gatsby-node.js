8c2443b639dc3d2ca827982aa2f4073f2b633146
Vinay Sagar Sharma <vinaysagar971995@gmail.com>
2019-02-23 08:44:13 +0530

    Update gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,6 +1,8 @@
-exports.onCreateNode = ({ node }) => {
+const { createFilePath } = require(`gatsby-source-filesystem`)
+
+exports.onCreateNode = ({ node, getNode }) => {
     if (node.internal.type === `MarkdownRemark`) {   
   	 const fileNode = getNode(node.parent)   
-  	 console.log(`\n`, fileNode.relativePath)
+  	 console.log(createFilePath({ node, getNode, basePath: `pages` }))
     }
 }
