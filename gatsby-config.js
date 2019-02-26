95dd269da9972caa6184f1316230fbd9aa79e553
Vinay Sagar Sharma <vinaysagar971995@gmail.com>
2019-02-26 18:01:57 +0530

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,21 @@ module.exports = {
 	 title: `Pandas Eating Lots`,  
 	},
   plugins: [
+  {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "GatsbyJS",
+        short_name: "GatsbyJS",
+        start_url: "/",
+        background_color: "#6b37bf",
+        theme_color: "#6b37bf",
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: "standalone",
+        icon: "src/images/icon.png", // This path is relative to the root of the site.
+      },
+    },
+    'gatsby-plugin-offline'
   {     
    resolve: `gatsby-source-filesystem`,   
       options: {    
@@ -10,7 +25,7 @@ module.exports = {
             path: `${__dirname}/src/`,  
         },  
     },
-	  `gatsby-transformer-remark`,
+    `gatsby-transformer-remark`,
     `gatsby-plugin-emotion`,
     {
       resolve: `gatsby-plugin-typography`,
